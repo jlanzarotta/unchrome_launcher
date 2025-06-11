6fe9df7d8948b962b294833cddc3174a5992a57b
jlanzarotta <JLanzarotta@matw.com>
2025-06-11 11:32:00 -0400

    feat: added application icon

---
+++ build.bat
@@ -0,0 +1,10 @@
+@echo off
+set CGO_ENABLED=0
+set GOOS=windows
+set GOARCH=amd64
+set BUILD_VERSION=1.0.4
+set BUILD_DATETIME="%date:~10,4%-%date:~4,2%-%date:~7,2%T%time: =0%"
+::go build -ldflags="-H windowsgui -s -w -X 'ungoogled_launcher/cmd.BuildDateTime=%BUILD_DATETIME%' -X 'ungoogled_launcher/cmd.BuildVersion=%BUILD_VERSION%'" -work -a -v -o ungoogled_launcher.exe main.go
+::go build -ldflags="-s -w -X 'ungoogled_launcher/cmd.BuildDateTime=%BUILD_DATETIME%' -X 'ungoogled_launcher/cmd.BuildVersion=%BUILD_VERSION%'" -work -a -v -o ungoogled_launcher.exe main.go
+::go build -ldflags="-X 'ungoogled_launcher/cmd.BuildDateTime=%BUILD_DATETIME%' -X 'ungoogled_launcher/cmd.BuildVersion=%BUILD_VERSION%'" -o ungoogled_launcher.exe main.go
+go build -ldflags="-X 'ungoogled_launcher/cmd.BuildDateTime=%BUILD_DATETIME%' -X 'ungoogled_launcher/cmd.BuildVersion=%BUILD_VERSION%'"
